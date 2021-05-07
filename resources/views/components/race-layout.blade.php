215dd6f6c467f77c14e50baeb31276733ac20119
Atiladanvi <atila.danvi@outlook.com>
2021-05-07 16:43:09 -0300

    Fix race layout

--- resources/views/components/race-layout.blade.php
+++ resources/views/components/race-layout.blade.php
@@ -3,7 +3,7 @@
         @livewire('race-header',  ['race' => $race])
     </x-slot>
     <hr>
-    <div class="md:flex flex-col md:flex-row md:min-h-screen w-full bg-gray-100">
+    <div class="md:flex max-w-7xl mx-auto flex-col md:flex-row md:min-h-screen w-full bg-gray-100">
         <x-octo-sidebar :items="$navigation" />
         <div class="flex bg-white px-4 flex-col w-full">
             {{ $slot }}
