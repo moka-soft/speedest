93e65ca91bac3dcd8a60b0426aaf927f2fa670b5
Atiladanvi <atila.danvi@outlook.com>
2021-05-12 12:01:35 -0300

    Remove race bg-white

--- resources/views/components/race-layout.blade.php
+++ resources/views/components/race-layout.blade.php
@@ -5,7 +5,7 @@
     <hr>
     <div class="md:flex max-w-7xl mx-auto flex-col md:flex-row md:min-h-screen w-full bg-gray-100">
         <x-octo-sidebar :items="$navigation" />
-        <div class="flex bg-white px-4 flex-col w-full">
+        <div class="flex px-4 flex-col w-full">
             {{ $slot }}
         </div>
     </div>
